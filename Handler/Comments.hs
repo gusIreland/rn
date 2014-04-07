9edec4759788411fadf1c90caf29363451949a23
Gus Ireland <gus.ireland@rackspace.com>
2014-04-06 21:38:06 -0500

    Fixed formatting and navbar

---
+++ Handler/Comments.hs
@@ -0,0 +1,9 @@
+module Handler.Comments where
+
+import Import
+
+getCommentsR :: Handler Html
+getCommentsR = error "Not yet implemented: getCommentsR"
+
+postCommentsR :: Handler Html
+postCommentsR = error "Not yet implemented: postCommentsR"
--- Handler/Post.hs
+++ Handler/Post.hs
@@ -1,10 +1,26 @@
 module Handler.Post
-    ( getPostR
-    , postPostR
-    )
 where
 
 import Import
+
+getPostsR :: Handler Html
+getPostsR = do
+    (postWidget, enctype) <- generateFormPost entryForm
+    defaultLayout $ do
+        $(widgetFile "entry")
+
+postPostsR :: Handler Html
+postPostsR = do
+    ((res,postWidget),enctype) <- runFormPost entryForm
+    case res of
+        FormSuccess post -> do
+            postId <- runDB $ insert post
+            setMessage $ toHtml $ (postTitle post) <> " created"
+            redirect $ HomeR 
+        _ -> defaultLayout $ do
+            setTitle "Please correct your entry form"
+            $(widgetFile "postAddError")
+
 
 getPostR :: PostId -> Handler Html
 getPostR postId = do
@@ -16,3 +32,9 @@ getPostR postId = do
 postPostR :: PostId -> Handler Html
 postPostR = error "not yet implemented"
 
+
+
+entryForm :: Form Post
+entryForm = renderDivs $ Post
+    <$> areq textField "Title" Nothing
+    <*> areq textField "link" Nothing
